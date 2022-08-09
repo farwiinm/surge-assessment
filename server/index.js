d4ac0ef73052c0281bc717b468fa6a5bea28ff28
farwiinm <farwiinm@gmail.com>
2022-08-09 11:40:26 +0530

    Server setup

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express=require('express');
+const dotenv=require('dotenv');
+const mongoose=require('mongoose');
+const cookieParser=require('cookie-parser');
+const cors=require('cors');
+const createUsers = require('./controllers/createUsers');
+const userRouter = require('../server/routes/userRouter')
+const notesRouter = require('../server/routes/noteListRouter');
+const authPage = require('./middlewares');
+dotenv.config();
+const corsOptions = {
+    exposedHeaders: 'auth-token',
+};
+  
+const app=express();
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+mongoose.set('runValidators', true);
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('Connected to DB'))
+    .catch(err => console.log('Error:', err));
+
+app.get('/create-user', createUsers);
+app.get('/', (req, res) => {
+    res.send('Home')
+})
+app.use('/', userRouter)
+app.use('/notes', notesRouter)
+
+app.listen(5000, () => console.log('Listening on port 5000'))
